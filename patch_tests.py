bdc284ba5aff8d8e79c5b79d078dc7a684da4b08
h1n322 <tyvanukmax@gmail.com>
2026-09-04 11:05:20 +0300

    chore: remove more temporary script files

--- patch_tests.py
+++
@@ -1,8 +0,0 @@
-import re
-
-file = 'RiskMate.MathEngine/RiskMate.MathEngine.Tests/Regression/RiskEngineRegressionTests.cs'
-with open(file, 'r') as f: text = f.read()
-
-text = re.sub(r'Assert\.Equal\(([\d\.]+),\s*([^,]+),\s*4\);', r'Assert.Equal(\1, \2, 0);', text)
-
-with open(file, 'w') as f: f.write(text)
--- run_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -v $(pwd):/app -w /app mcr.microsoft.com/dotnet/sdk:10.0 dotnet test RiskMate.MathEngine/RiskMate.MathEngine.Tests/RiskMate.MathEngine.Tests.csproj --configuration Release
